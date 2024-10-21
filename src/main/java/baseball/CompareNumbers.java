5a6cacfcbbfeb93c74d942cb22768e8b9c6fb546
thsamajiki <ant1102@naver.com>
2024-10-21 15:16:34 +0900

    feat: 입력된 수와 컴퓨터가 생성한 난수를 비교하는 기능 추가

--- src/main/java/baseball/CompareNumbers.java
+++ src/main/java/baseball/CompareNumbers.java
@@ -6,6 +6,35 @@ public class CompareNumbers {
   private boolean gameOver;
 
   public void compareNumbers(Player player, Computer computer) {
+    List<Integer> randomNumberList = computer.getRandomNumberList();
     player.playerInputNumber(); // 플레이어가 숫자를 입력
+    List<Integer> playerNumberList = player.getInputNumberList();
+
+    int strikes = calculateStrikes(randomNumberList, playerNumberList);
+    int balls = calculateBalls(randomNumberList, playerNumberList);
+  }
+
+  private int calculateStrikes(List<Integer> randomNumberList, List<Integer> playerNumberList) {
+    int strikes = 0;
+
+    for (int i = 0; i < randomNumberList.size(); i++) {
+      if (randomNumberList.get(i).equals(playerNumberList.get(i))) {
+        strikes++;
+      }
+    }
+
+    return strikes;
+  }
+
+  private int calculateBalls(List<Integer> randomNumberList, List<Integer> playerNumberList) {
+    int balls = 0;
+
+    for (int i = 0; i < randomNumberList.size(); i++) {
+      if (randomNumberList.contains(playerNumberList.get(i)) && !randomNumberList.get(i).equals(playerNumberList.get(i))) {
+        balls++;
+      }
+    }
+
+    return balls;
   }
 }
