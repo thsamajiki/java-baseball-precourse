3870332efa559672b662e91299fbcf02ae955be6
thsamajiki <ant1102@naver.com>
2024-10-21 15:11:58 +0900

    feat: 컴퓨터가 1에서 9까지 서로 다른 임의의 3자리의 난수를 생성하는 기능 추가

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,33 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Computer {
+  private List<Integer> randomNumberList;
+
+  public Computer() {
+    this.randomNumberList = generateRandomNumberList();
+  }
+
+  private List<Integer> generateRandomNumberList() {
+    Set<Integer> notDuplicatedNumbers = new HashSet<>();
+
+    while (notDuplicatedNumbers.size() < 3) {
+      int num = Randoms.pickNumberInRange(1, 9); // 1 ~ 9 난수 생성
+
+      notDuplicatedNumbers.add(num);
+    }
+
+    return new ArrayList<>(notDuplicatedNumbers);
+  }
+
+
+  public List<Integer> getRandomNumberList() {
+    return this.randomNumberList;
+  }
+}
